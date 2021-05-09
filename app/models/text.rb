2b4887207f2c2db602aa776e0d34265667998c6c
Issei <chatani171@gmail.com>
2021-05-09 13:20:24 +0900

    Text, Movieモデルのgenreカラムを「列挙型」へ

--- app/models/text.rb
+++ app/models/text.rb
@@ -4,4 +4,14 @@ class Text < ApplicationRecord
     validates :title
     validates :content
   end
+
+  enum genre: {
+    invisible: 0,
+    basic: 1,
+    git: 2,
+    ruby: 3,
+    rails: 4,
+    php: 5
+  }
+
 end
