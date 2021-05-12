73cc0c226da74d530618ba75e633d4d71095ff01
y.watanabe <trance3912hkss@gmail.com>
2021-05-12 16:48:29 +0900

    devise を使用したモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_041905) do
+ActiveRecord::Schema.define(version: 2021_05_12_070514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 2021_05_12_041905) do
     t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_texts_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_texts_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
