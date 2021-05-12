f23eb4d58cdefe775debe0de88bcff34552ba5ce
yasu-grz <yatthun82@gmail.com>
2021-05-12 19:10:55 +0900

    教材のCSVインポート機能を実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+Text.destroy_all
+CSV.foreach("db/csv_data/text_data.csv", headers: true) do |row|
+  Text.create(row.to_h)
+end
+puts "テキスト教材の初期データインポート完了！"
+
+Movie.destroy_all
+CSV.foreach("db/csv_data/movie_data.csv", headers: true) do |row|
+  Movie.create(row.to_h)
+end
+puts "動画教材の初期データインポート完了！"
