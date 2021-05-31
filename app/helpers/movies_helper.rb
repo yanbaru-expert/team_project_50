4da61072285a933eeb126f384d47d3beed62488e
y.watanabe <trance3912hkss@gmail.com>
2021-05-31 20:09:52 +0900

    youtube動画のサイズを修正

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,8 +1,8 @@
 module MoviesHelper
   def embed_youtube(url)
     tag.iframe(
-      width: 368,
-      height: 235,
+      width: 560,
+      height: 315,
       src: url,
       frameborder: 0,
       allow: "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture",
