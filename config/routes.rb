4984e201b7f33c9fe50939e93a3e6171406ab0e6
yasu-grz <yatthun82@gmail.com>
2021-06-04 13:52:22 +0900

    ゲストログイン機能を実装、ナビバーと「ログイン・新規登録のページ」に配置

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   root "texts#index"
+    devise_scope :user do
+  post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
   resources :texts, only: [:index, :show]
   resources :movies, only: [:index]
 end
