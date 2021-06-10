383eaa09dc467271cbdd35e0172f77691cdd25ab
akihiro <aakihiro0434@gmail.com>
2021-06-11 07:49:39 +0900

    マジックナンバーを定数に変更

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,7 @@
 class MoviesController < ApplicationController
+  PER_PAGE = 12
   def index
     # @movies = Movie.where(genre: Movie::RAILS_GENRE_LIST).page(params[:page]).per(12)
-    @movies = Movie.filter_by(params[:genre]).page(params[:page]).per(12)
+    @movies = Movie.filter_by(params[:genre]).page(params[:page]).per(PER_PAGE)
   end
 end
