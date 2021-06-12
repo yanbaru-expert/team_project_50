81a141e0ab6e7e7b339c5812a7f6d879c71e5226
y.watanabe <trance3912hkss@gmail.com>
2021-06-12 20:46:52 +0900

    PHPテキスト教材ページをジャンル分け

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,7 +1,6 @@
-
 class TextsController < ApplicationController
   def index
-    @texts = Text.where(genre: Text::RAILS_GENRE_LIST)
+    @texts = Text.filter_by(params[:genre])
   end
 
   def show
