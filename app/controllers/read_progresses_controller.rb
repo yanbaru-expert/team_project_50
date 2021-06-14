7922a3afa9ca2aebbf34f2415054eb192a3abb0d
Issei <chatani171@gmail.com>
2021-06-14 23:55:46 +0900

    不要なコメントアウトを削除

--- app/controllers/read_progresses_controller.rb
+++ app/controllers/read_progresses_controller.rb
@@ -2,12 +2,10 @@ class ReadProgressesController < ApplicationController
   def create
     @text = Text.find(params[:text_id])
     current_user.read_progresses.create!(text_id: @text.id)
-    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @text = Text.find(params[:text_id])
     current_user.read_progresses.find_by(text_id: @text.id).destroy!
-    # redirect_back(fallback_location: root_path)
   end
 end
