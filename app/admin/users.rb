aa2ab00ef6de59b9de52b8ecacf97cd365c94d49
Issei <chatani171@gmail.com>
2021-06-22 22:31:31 +0900

    管理者画面から教材を編集できるように修正

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -35,7 +35,7 @@ ActiveAdmin.register User do
   # or
   #
   # permit_params do
-  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at]
+    # permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
